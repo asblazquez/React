1ffd0b26a3309adace8e3bb8110f4e446a04c1af
asblazquez <asanchezb23@gmail.com>
2022-03-09 14:31:48 +0100

    Header con boton

--- BancoReact/src/components/Header.js
+++ BancoReact/src/components/Header.js
@@ -1,13 +1,17 @@
 import React,{Component} from "react";
+import Button from "react-bootstrap/esm/Button";
 
 class Componente extends Component{
     //Permite rendeizar el código js
     render(){
         return(
             <div class = "row bg-dark fixed-top">
-                <div class = "col-md-12">
-                    <div class = "page-header text-light">
-                        <h1> Banco Santondor </h1>
+                <div class = "row">
+                    <div class = "col-4 text-center">
+                    <button type="button" class="btn btn-secondary">Secondary</button>
+                    </div>
+                    <div class = "col-4 text-light text-center">
+                        <h1>Banco Santondor</h1>
                     </div>
                 </div>
             </div>
