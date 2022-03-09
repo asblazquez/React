a306de58bf4d3e079a3bf5454fc819313d422ea4
Alex <alejandrotauste@gmail.com>
2022-03-09 17:24:31 +0100

    MultiPáginas2

--- BancoReact/src/App.js
+++ BancoReact/src/App.js
@@ -2,17 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 //Importamos el componente creado
 import Componente from './components/Componente';
+import MainComponent from './components/MainComponent';
 import Header from './components/Header';
 import Button from 'react-bootstrap/Button';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import {Switch,Route,Redirect,withRouter} from 'react-router-dom'
 
 function App() {
   return (
     <div className="container">
-      <Header/>
-      {/* Sección nueva */}
+      <BrowserRouter>
+      <Main/>
+      </BrowserRouter>
+      
     </div>
-    
+      
   );
 }
 
-export default App;
+export default withRouter(App);
---
+++ BancoReact/src/components/Login.js
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+
+
+function Login(){
+    return(
+        <div>Nombre y Contraseña</div>    
+    )
+}
+
+export default Login;
