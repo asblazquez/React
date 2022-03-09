d352b280e3b11f6c2690ebe39cc44fff762801ce
asblazquez <asanchezb23@gmail.com>
2022-03-09 13:35:24 +0100

    Header fixed top

--- BancoReact/src/App.js
+++ BancoReact/src/App.js
@@ -2,36 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 //Importamos el componente creado
 import Componente from './components/Componente';
+import Header from './components/Header';
 import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="container">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Eres mas manco que Yair17
-        </p>
-        <p>Eres una bonita persona a.MIC, y gallo es gay</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <br></br>
-        <button type="button" class="btn btn-danger">
-          Boton de prueba
-        </button>
-      </header>
+      <Header/>
       {/* Sección nueva */}
-      <section>
-        {/* Simplificación del componente */}
-        <Componente/>
-      </section>
     </div>
+    
   );
 }
 
---
+++ BancoReact/src/components/Header.js
@@ -0,0 +1,18 @@
+import React,{Component} from "react";
+
+class Componente extends Component{
+    //Permite rendeizar el código js
+    render(){
+        return(
+            <div class = "row bg-dark fixed-top">
+                <div class = "col-md-12">
+                    <div class = "page-header text-light">
+                        <h1> Banco Santondor </h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Componente;
