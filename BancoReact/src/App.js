4945a761950b828f6c767426a04e693c53aace94
Alex <alejandrotauste@gmail.com>
2022-03-10 11:38:13 +0100

    Yarn problemas

--- BancoReact/src/App.js
+++ BancoReact/src/App.js
@@ -4,19 +4,19 @@ import './App.css';
 import Componente from './components/Componente';
 import MainComponent from './components/MainComponent';
 import Header from './components/Header';
-import ImageSlider from './components/ImageSlider';
+import Main from './components/MainComponent';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import {Switch,Route,Redirect,withRouter} from 'react-router-dom'
 
 function App() {
   return (
     <div class = "bg-secondary">
       <div className="container">
         <div>
-          <Header/>
-          <ImageSlider/>
+          <BrowserRouter>
+          <Main/>
+          </BrowserRouter>
         </div> 
       </div>
     </div>
@@ -25,4 +25,4 @@ function App() {
   );
 }
 
-export default withRouter(App);
+export default (App);
